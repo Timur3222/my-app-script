51433fc7818e50f40f0fff474dac6e5795254b95
Timur <rt1996xzx@yandex.ru>
2024-08-06 09:53:58 +0300

    commit

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 window.onesignal = typeof onesignal === "undefined" ? null : onesignal;
 
 function onDeviceReady() {
+  alert(navigator)
   let APP_URL = site.trim().replace(/\/+$/, "");
 
   if (APP_URL == "https://hoshinowa.ru" && window.DEVELOPMENT) {
