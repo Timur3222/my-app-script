ea91a15f579c9e1fa82925065ce036db73fea78d
Timur <rt1996xzx@yandex.ru>
2024-08-06 10:50:42 +0300

    commit

--- index.js
+++ index.js
@@ -385,6 +385,7 @@ function App(appUrl) {
         break;
 
       case "open":
+        alert(JSON.stringify(messageData, null, 2))
         this.app = cordova.InAppBrowser.open(
           messageData.url,
           messageData.target,
